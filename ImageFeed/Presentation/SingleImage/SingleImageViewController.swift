601bb4196d2f53dcd0d3cdb29f750b7d1f6932de
Evo Eclipse <evo.galaio@outlook.com>
2025-03-20 01:17:30 +0300

    Add image zooming and sharing
    - Wrapped Image View in Scroll View to enable image scaling functionality
    - Implemented automatic image scaling and centering logic
    - Added Share Button assets and sharing functionality

--- ImageFeed/Presentation/SingleImage/SingleImageViewController.swift
+++ ImageFeed/Presentation/SingleImage/SingleImageViewController.swift
@@ -5,26 +5,107 @@ final class SingleImageViewController: UIViewController {
     // MARK: - IB Outlets
     
     @IBOutlet private var imageView: UIImageView!
+    @IBOutlet var scrollView: UIScrollView!
     
     // MARK: - Public Properties
     
     var image: UIImage? {
         didSet {
-            guard isViewLoaded else { return }
+            guard isViewLoaded, let image else { return }
+
             imageView.image = image
+            imageView.frame.size = image.size
+            updateZoomAndCenterImage(animated: false)
         }
+    }
+    
+    // MARK: - Private Properties
+    
+    private func updateZoomAndCenterImage(animated: Bool) {
+        guard let image = imageView.image else { return }
+        
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let minScale = min(hScale, vScale)
+        
+        let scale = min(scrollView.maximumZoomScale, max(scrollView.minimumZoomScale, minScale))
+        
+        if scrollView.zoomScale != scale {
+            scrollView.setZoomScale(scale, animated: animated)
+        }
+        
+        centerScrollViewContents()
+    }
+    
+    private func centerScrollViewContents() {
+        let contentSize = scrollView.contentSize
+        let scrollViewSize = scrollView.bounds.size
+        
+        var horizontalInset: CGFloat = 0
+        var verticalInset: CGFloat = 0
+        
+        if contentSize.width < scrollViewSize.width {
+            horizontalInset = (scrollViewSize.width - contentSize.width) / 2
+        }
+        
+        if contentSize.height < scrollViewSize.height {
+            verticalInset = (scrollViewSize.height - contentSize.height) / 2
+        }
+        
+        scrollView.contentInset = UIEdgeInsets(
+            top: verticalInset,
+            left: horizontalInset,
+            bottom: verticalInset,
+            right: horizontalInset
+        )
     }
     
     // MARK: - Overrides Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        
+        guard let image else { return }
         imageView.image = image
+        imageView.frame.size = image.size
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        updateZoomAndCenterImage(animated: false)
+    }
+
     
     // MARK: - IB Actions
     
     @IBAction private func didTapBackButton() {
         dismiss(animated: true, completion: nil)
+    }
+    @IBAction func didTapShareButton(_ sender: Any) {
+        guard let image = imageView.image else { return }
+        let share = UIActivityViewController(
+            activityItems: [image],
+            applicationActivities: nil
+        )
+        present(share, animated: true, completion: nil)
+    }
+}
+
+// MARK: - UIScrollViewDelegate
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        centerScrollViewContents()
     }
 }
